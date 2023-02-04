acff524ca8bc5ff5e3e75e35dbc4d5bbff5e6350
begalievn <begaliev.info@gmail.com>
2023-02-04 13:19:28 +0600

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Girls4Girls')
+    .setDescription('API for girls4girls me')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
