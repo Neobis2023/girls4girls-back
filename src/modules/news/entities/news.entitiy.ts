2649b73c7f5b2cb017ebe90dae869c58042c03c4
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2023-02-24 21:56:33 +0600

    fixed validation

--- src/modules/news/entities/news.entitiy.ts
+++ src/modules/news/entities/news.entitiy.ts
@@ -1,20 +1,31 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { BaseEntity } from 'src/base/base.entity';
 import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class NewsEntity extends BaseEntity {
   @Column()
+  @IsNotEmpty()
+  @IsString()
   title: string;
 
   @Column()
+  @IsNotEmpty()
+  @IsString()
   description: string;
 
   @Column()
+  @IsNotEmpty()
+  @IsNumber()
   imageId: number;
 
   @Column()
+  @IsNotEmpty()
+  @IsNumber()
   commentId: number;
 
   @Column()
+  @IsNotEmpty()
+  @IsNumber()
   likeId: number;
 }
