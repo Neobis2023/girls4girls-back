7c2e06d07a90b5cd66e8eaaeb46b1e87434e66ee
Tolgonai022002 <tokaldomotova@gmail.com>
2023-02-24 02:03:05 -0900

    Added forum module

---
+++ src/modules/forum/entities/forum.entity.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { BaseEntity } from "src/base/base.entity";
+import { Image } from "src/modules/image/entities/image.entity";
+import { Column, Entity, OneToMany } from "typeorm";
+
+@Entity()
+export class Forum extends BaseEntity {
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    title: string
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    description: string
+
+    @Column()
+    @IsNotEmpty()
+    @IsString()
+    address: string
+
+    @OneToMany(()=>Image,(image)=>image.forum, {cascade: true} )
+    @IsOptional()
+    image: Image []
+}
